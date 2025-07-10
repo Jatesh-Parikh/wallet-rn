f183be1fcba9d29913651be801affc74761139b8
Jatesh Parikh <jateshparikh97@gmail.com>
2025-07-10 20:45:43 +0530

    home screen completed

---
+++ mobile/app/(root)/index.jsx
@@ -0,0 +1,110 @@
+import { useUser } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+import {
+  Alert,
+  Flatlist,
+  Image,
+  RefreshControl,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SignOutButton } from "@/components/SignoutButton";
+import { useTransactions } from "../../hooks/useTransactions";
+import { useEffect, useState } from "react";
+import PageLoader from "../../components/PageLoader";
+import { styles } from "../../assets/styles/home.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { BalanceCard } from "../../components/BalanceCard";
+import { TransactionItem } from "../../components/TransactionItem";
+import NoTransactionFound from "../../components/NoTransactionsFound";
+
+export default function Page() {
+  const { user } = useUser();
+  const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(user.id);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadData();
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  const handleDelete = async (id) => {
+    Alert.alert(
+      "Delete transaction?",
+      "Are you sure you want to delete this transaction?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          style: "destructive",
+          onPress: () => deleteTransaction(id),
+        },
+      ]
+    );
+  };
+
+  if (isLoading && !refreshing) return <PageLoader />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#fff" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+
+        <BalanceCard summary={summary} />
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      <Flatlist
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        renderItem={({ item }) => (
+          <TransactionItem item={item} onDelete={handleDelete} />
+        )}
+        ListEmptyComponent={<NoTransactionFound />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </View>
+  );
+}
