3ef02a2ed5893ce33f4ae50b47f73c97fd227bba
Jatesh Parikh <jateshparikh97@gmail.com>
2025-07-10 17:28:39 +0530

    useTransactions hook completed

---
+++ mobile/hooks/useTransactions.js
@@ -0,0 +1,63 @@
+import { useCallback, useState } from "react";
+import { Alert } from "react-native";
+import { API_URL } from "../constants/api";
+
+export const useTransactions = (userId) => {
+  const [transactions, setTransactions] = useState([]);
+  const [summary, setSummary] = useState({
+    balance: 0,
+    income: 0,
+    expenses: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${userId}`);
+      const data = await response.json();
+      setTransactions(data);
+    } catch (error) {
+      console.log("Error fetching transactions:", error.message);
+    }
+  }, [userId]);
+
+  const fetchSummary = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/summary/${userId}`);
+      const data = await response.json();
+      setSummary(data);
+    } catch (error) {
+      console.log("Error fetching summary:", error.message);
+    }
+  }, [userId]);
+
+  const loadData = useCallback(async () => {
+    if (!userId) return;
+
+    setIsLoading(true);
+    try {
+      await Promise.all([fetchTransactions(), fetchSummary()]);
+    } catch (error) {
+      console.error("Error loading data:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [fetchTransactions, fetchSummary, userId]);
+
+  const deleteTransaction = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) throw new Error("Failed to delete the transaction");
+
+      loadData(); // refreshing the data after deletion
+      Alert.alert("Success", "Transaction deleted successfully");
+    } catch (error) {
+      console.error("Error deleting the transaction:", error.message);
+      Alert.alert("Error", error.message);
+    }
+  };
+
+  return { transactions, summary, isLoading, loadData, deleteTransaction };
+};
